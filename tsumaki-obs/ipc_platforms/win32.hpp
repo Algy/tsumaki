4951f1def5cc18c61a5fda71133d1f25e3a0bfe0
Algy <a9413miky@gmail.com>
2019-04-07 01:56:38 +0900

    First successful build in win32

--- tsumaki-obs/ipc_platforms/win32.hpp
+++ tsumaki-obs/ipc_platforms/win32.hpp
@@ -3,9 +3,17 @@
 
 namespace tsumaki {
     class Win32IPC : public IPC {
+    private:
+        DWORD pid = -1;
+    public:
         using IPC::IPC;
         virtual bool check_process();
         virtual bool spawn_process();
         virtual void terminate_process();
+	virtual void sleep(unsigned int milliseconds);
+
+    private:
+	    int find_pid();
+	    std::wstring get_pid_file_path();
     };
 }
