2d55e87d0d86a0deb0766771f2d9b40ce050bfe0
Algy <algy@ubuntu.ubuntu-domain>
2019-04-06 22:18:32 +0900

    standardization

--- tsumaki-obs/tsumaki-plugin.cpp
+++ tsumaki-obs/tsumaki-plugin.cpp
@@ -40,59 +40,67 @@ static struct obs_source_frame* do_filter(TsumakiFilter* filter, struct obs_sour
 
 std::atomic<int> filter_creation_count(0);
 
-struct obs_source_info tsumaki_filter = {
-	.id = "tsumaki_filter",
-	.type = OBS_SOURCE_TYPE_FILTER,
-	.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_ASYNC,
-	.get_name = [] (void *unused) { UNUSED_PARAMETER(unused); return obs_module_text("Tsumaki"); },
-	.create = [] (obs_data_t *settings, obs_source_t *context) {
-        TsumakiFilter *filter = new TsumakiFilter();
-        filter->set_context(context);
-        filter->init();
-        filter->update_settings(settings);
+struct obs_source_info tsumaki_filter;
 
-        if (filter_creation_count++ == 0) {
-            filter->run_once();
-        }
-        filter->debug << "tsumaki has been initialized" << filter->debug.endl;
-        return static_cast<void *>(filter);
-    },
-	.destroy = [] (void *data) {
-        TsumakiFilter *filter = static_cast<TsumakiFilter *>(data);
-        filter->destroy();
-        delete filter;
-    },
-	.get_properties = [] (void *data) {
-        TsumakiFilter *filter = static_cast<TsumakiFilter *>(data);
-        obs_properties_t *props = obs_properties_create();
-        filter->get_properties(props);
-        filter->debug << "properties have been initialized" << filter->debug.endl;
-        return props;
-    },
-	.update = [] (void *data, obs_data_t *settings) {
-        TsumakiFilter *filter = static_cast<TsumakiFilter *>(data);
-        filter->update_settings(settings);
-        filter->info << "settings updated" << filter->info.endl;
-    },
-	.filter_video = [] (void *data, struct obs_source_frame *obs_frame) {
-        TsumakiFilter* filter = static_cast<TsumakiFilter *>(data);
+const char* get_name(void *unused) {
+    UNUSED_PARAMETER(unused);
+    return obs_module_text("Tsumaki");
+}
 
-        auto fn = [] { return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); };
+void* create(obs_data_t *settings, obs_source_t *context) {
+    TsumakiFilter *filter = new TsumakiFilter();
+    filter->set_context(context);
+    filter->init();
+    filter->update_settings(settings);
 
-        auto t1 = fn();
-        struct obs_source_frame* filtered_obs_frame = do_filter(filter, obs_frame);
-        auto t2 = fn();
-        // filter->info << "MS: " << (int)(t2 - t1) << filter->info.endl;
-        return filtered_obs_frame;
-    },
-	.filter_remove = [] (void *data, obs_source_t *parent) {
-        static_cast<TsumakiFilter *>(data)->detach(parent);
+    if (filter_creation_count++ == 0) {
+        filter->run_once();
     }
+    filter->debug << "tsumaki has been initialized" << filter->debug.endl;
+    return static_cast<void *>(filter);
+}
+
+void destroy(void *data) {
+    TsumakiFilter *filter = static_cast<TsumakiFilter *>(data);
+    filter->destroy();
+    delete filter;
+}
+
+obs_properties_t* get_properties(void *data) {
+    TsumakiFilter *filter = static_cast<TsumakiFilter *>(data);
+    obs_properties_t *props = obs_properties_create();
+    filter->get_properties(props);
+    filter->debug << "properties have been initialized" << filter->debug.endl;
+    return props;
+}
+
+struct obs_source_frame* filter_video(void *data, struct obs_source_frame *obs_frame) {
+    TsumakiFilter* filter = static_cast<TsumakiFilter *>(data);
+
+    auto fn = [] { return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count(); };
+
+    auto t1 = fn();
+    struct obs_source_frame* filtered_obs_frame = do_filter(filter, obs_frame);
+    auto t2 = fn();
+    // filter->info << "MS: " << (int)(t2 - t1) << filter->info.endl;
+    return filtered_obs_frame;
+}
+void filter_remove(void *data, obs_source_t *parent) {
+    static_cast<TsumakiFilter *>(data)->detach(parent);
 };
 
 
 extern "C" {
     bool obs_module_load(void) {
+        tsumaki_filter.id = "tsumaki_filter";
+        tsumaki_filter.type = OBS_SOURCE_TYPE_FILTER;
+        tsumaki_filter.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_ASYNC;
+        tsumaki_filter.get_name = get_name;
+        tsumaki_filter.create = create;
+        tsumaki_filter.destroy = destroy;
+        tsumaki_filter.get_properties = get_properties;
+        tsumaki_filter.filter_video = filter_video;
+        tsumaki_filter.filter_remove = filter_remove;
         obs_register_source(&tsumaki_filter);
         return true;
     }
